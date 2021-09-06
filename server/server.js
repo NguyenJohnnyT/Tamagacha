11f175c5e2f8d5b4100c74be9f7442b0b0255a64
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-09-06 13:23:27 -0700

    export bug fix

--- server/server.js
+++ server/server.js
@@ -12,9 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 3005;
 
 const dev = process.env.NODE_ENV !== 'production'
-export const SERVER = dev ? `http://localhost:${PORT}` : 'https://hidden-basin-34192.herokuapp.com/'
+const SERVER = dev ? `http://localhost:${PORT}` : 'https://hidden-basin-34192.herokuapp.com/'
 
-const SERVER = 
 // -- cron -- \\
 //!Tweak schedule based on game balance
 cron.schedule('0,05,10,15,20,25,30,35,40,45,50,55 * * * * *', () => {
@@ -39,3 +38,5 @@ app.get('*', (req, res) => {
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
+
+module.exports = { SERVER }
