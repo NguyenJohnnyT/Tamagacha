0ea5d57d3409098edbe1dc509414cef6f647df8a
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-06-25 11:36:35 -0700

    make app active once a day

--- server/utils/upkeep_schedule.js
+++ server/utils/upkeep_schedule.js
@@ -1,13 +1,13 @@
 //!Tweak with numbers for gameplay balance.  These numbers are used to subtract from upkeep.  For use in passive.js
 const upkeepVals = {
-  hunger: 1,
-  bladder: 1,
+  hunger: 10,
+  bladder: 15,
   happiness: 20,
-  status: 1
+  status: 10
 }
 
 //!Tweak schedule based on game balance. For use in server.js
-const schedule = '*/10 * * * * *'
+const schedule = '* 0 * * * *' // (second) minute hour [day of month] [month] [day of week]
 
 module.exports = {
   upkeepVals,
